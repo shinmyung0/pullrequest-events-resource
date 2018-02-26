39c22f8f11dc69503580059076ee4b77b540a6df
Shin Yoon <shiyoon@stubhub.com>
2018-02-26 00:52:40 -0800

    Added docker build only on tagged commits

--- build.sh
+++ build.sh
@@ -3,4 +3,13 @@
 
 cd scripts && yarn && yarn run test:all && cd ../
 
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" && docker build -t shinmyung0/pullrequest-events-resource:latest . && docker push shinmyung0/pullrequest-events-resource:latest
+if [[ ! -z "$TRAVIS_TAG" ]]; 
+then
+    echo "This is a tagged Build"
+    echo "TRAVIS_TAG=${TRAVIS_TAG}"
+    echo "Publishing to Docker Hub..."
+    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" && \
+    docker build -t shinmyung0/pullrequest-events-resource:${TRAVIS_TAG} -t shinmyung0/pullrequest-events-resource:latest . && \
+    docker push shinmyung0/pullrequest-events-resource:${TRAVIS_TAG} && \
+    docker push shinmyung0/pullrequest-events-resource:latest
+fi
