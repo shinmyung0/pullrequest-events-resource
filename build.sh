056c583106e25d8f4a428f4c4db1a129cc03e5b1
Shin Yoon <shiyoon@stubhub.com>
2018-02-26 00:05:53 -0800

    Added dockerignore and docker build as part of build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+
 cd scripts && yarn && yarn run test:all && cd ../
 
-# docker build -t shinmyung0/pullrequest-events-resource:latest . && docker push shinmyung0/pullrequest-events-resource:latest
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" && docker build -t shinmyung0/pullrequest-events-resource:latest . && docker push shinmyung0/pullrequest-events-resource:latest
